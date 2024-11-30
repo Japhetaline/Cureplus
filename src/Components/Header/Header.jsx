80593436c35c8f1e785da78b52d3931836eb2d1e
Japhetaline <japhetdamassoh@gmail.com>
2024-11-30 23:36:36 +0100

    Cureplus

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,15 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import Logo from "../Images/logo.png";
-import '../Header/Header.css'
-
+import "../Header/Header.css";
 
 export const Header = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const handleToggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
     <nav className="navbar">
       <div className="navbar-logo">
         <img src={Logo} alt="CurePlus Medicals" />
       </div>
-      <ul className="navbar-links">
+      <button className="navbar-toggle" onClick={handleToggleMenu}>
+        ☰
+      </button>
+      <ul className={`navbar-links ${isMenuOpen ? "open" : ""}`}>
         <li><a href="#home">Home</a></li>
         <li><a href="#services">Services</a></li>
         <li><a href="#about">About</a></li>
